338fe277dfdeea65b32f8d77dd5ffd351b8a3a08
Greenweb <Greenweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-05 17:07:21 +0000

    Updated version number
    git-svn-id: https://plugins.svn.wordpress.org/easy-heads-up-bar/trunk@943723 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- easy-heads-up-bar.php
+++ easy-heads-up-bar.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy Heads Up Bar
 Plugin URI: http://www.beforesite.com/plugins/easy-heads-up-bar
 Description: An Easy to use notification (heads up) bar for your WordPress website with a linked call to action
-Version: 2.1.2
+Version: 2.1.3
 Author: Greenweb
 Author URI: http://www.greenvillweb.us 
 */
@@ -51,7 +51,7 @@ Author URI: http://www.greenvillweb.us
     $ehb_plugin_loc = preg_replace( '/^https/', 'http', $ehb_plugin_loc );
   
   define( 'EHB_URL',            plugin_dir_url(__FILE__) );
-  define( 'EHB_VERSION',        '2.1.2' );
+  define( 'EHB_VERSION',        '2.1.3' );
   
   include 'lib/ehb-utility-functions.php';
   include 'lib/ehb-admin.php';
